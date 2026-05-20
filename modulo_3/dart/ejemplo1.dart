b837688566f0bfe11a7ccc230691f479b7cbe101
unknown <heymiced34@gmail.com>
2026-05-19 20:22:56 -0500

    Clase/19/05/2026

--- modulo_3/dart/ejemplo1.dart
+++ modulo_3/dart/ejemplo1.dart
@@ -3,8 +3,7 @@ import 'dart:io';
 void main() {
   stdout.write('Ingrese el numero para la tabla de multiplicar: ');
   int numero = int.parse(stdin.readLineSync()!);
-
-  print('\nTabla de multiplicar del $numero:');
+  print('Tabla de multiplicar del $numero:');
   for (int i = 1; i <= 10; i++) {
     print('$numero x $i = ${numero * i}');
   }
